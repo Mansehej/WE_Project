bb775f1b6702d4c662386133087039fa409273cb
Mansehej Singh <mansehej@gmail.com>
2020-05-24 00:24:05 +0530

    Implement dynamic routing from movie lists

--- src/views/sections-sections/SectionBlog.js
+++ src/views/sections-sections/SectionBlog.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 // reactstrap components
 import {
@@ -18,47 +18,27 @@ import { db } from '../../firebase';
 
 function MoviesList({ movie }) {
 
-  const [redirect, setRedirect] = useState()
-  let url
-
-  console.log(redirect)
-
-  function formURL (props) {
-    console.log(props)
-    url='/'+props
-    
-  if(redirect == true) {
-    return <Redirect to={url} />
-  }
-  else
-  setRedirect(false)
-
-}
-
   return (
     <>
-      <Col md="4" style={{minWidth: "300px", marginBottom: "50px"}}>
-        <Card className="card-blog"  onClick={ () => formURL(movie.id), setRedirect(true)}>
+      <Col md="4" style={{ minWidth: "300px", marginBottom: "50px" }}>
+        <Card className="card-blog">
           <div className="card-image">
-            <a href="#pablo" >
               <img
                 alt="..."
                 className="img img-raised"
                 src={require("assets/img/sections/miguel-perales.jpg")}
               />
-            </a>
+            
           </div>
           <CardBody>
             <h6 className="card-category text-info">{movie.genres}</h6>
             <CardTitle tag="h5">
-              <a href="#pablo" >
                 {movie.title}
-              </a>
             </CardTitle>
             <hr />
             <CardFooter>
               <div className="author">
-                  <span>{movie.director}</span>
+                <span>{movie.director}</span>
               </div>
               <div className="stats">
                 Rating: {movie.rating}
@@ -76,37 +56,45 @@ function MoviesList({ movie }) {
 function SectionBlog() {
 
   let allMovies = []
-  const [movies, setMovies] = useState([])
-  
-  if (Object.keys(movies).length == 0) {
+  const [movies, setMovies] = useState({
+    movies: []
+  })
+  const [redirect, setRedirect] = useState('')
+
+  if (redirect.length != 0) {
+    return <Redirect to={redirect} />
+  }
+
+  function redirectTo(id) {
+    setRedirect('/movie/' + id)
+  }
+
+  if (Object.keys(movies.movies).length == 0) {
     db.collection('movie').orderBy('rating', "desc").limit(10).get().then(docs => {
       docs.forEach(doc => {
         let movieObj = doc.data()
         movieObj['id'] = doc.id
         allMovies.push(movieObj)
       })
-      setMovies(allMovies)
-      console.log(allMovies)
-
+      setMovies({ movies: allMovies })
     })
   }
-
 
   return (
     <div >
       <div className="section secion-blog cd-section" id="blogs" >
         <div className="blog-2 section section-gray">
           <Container>
-          <h2 className="title">Movies List</h2>
+            <h2 className="title">Movies List</h2>
 
             <Col className="ml-auto mr-auto" md="10">
-              <br />           
-              {movies.map((movie, index) => (
-                <div >
-                <MoviesList
-                  key={index}
-                  movie={movie}
-                />
+              <br />
+              {movies.movies.map((movie, index) => (
+                <div onClick={() => { redirectTo(movie.id) }}>
+                  <MoviesList
+                    key={index}
+                    movie={movie}
+                  />
                 </div>
               ))}
             </Col>
