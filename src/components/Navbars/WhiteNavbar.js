acc1591c97c60a79bbab91ae310cc674a28c3e35
Mansehej Singh <mansehej@gmail.com>
2020-05-24 01:03:17 +0530

    Add homepage link to MovIT title in navbar

--- src/components/Navbars/WhiteNavbar.js
+++ src/components/Navbars/WhiteNavbar.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 // JavaScript plugin that hides or shows a component based on your scroll
 import Headroom from "headroom.js";
 // reactstrap components
 import {
-  Button,
-  Collapse,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  NavbarBrand,
   Navbar,
-  NavItem,
-  Nav,
   Container,
-  UncontrolledTooltip
 } from "reactstrap";
 // core components
 
@@ -32,9 +21,11 @@ function WhiteNavbar() {
       <Navbar className="fixed-top" expand="lg" id="navbar-main">
         <Container>
           <div className="navbar-translate">
-            <h3 style={{marginBottom: "20px"}}>MovIT</h3>
+            <a href="/index">
+              <h3 style={{ marginBottom: "20px" }}>MovIT</h3>
+            </a>
           </div>
-          </Container>
+        </Container>
       </Navbar>
     </>
   );
